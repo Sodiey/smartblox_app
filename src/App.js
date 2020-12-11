32a514c653cae9d48bd0f6b39cc0bc222a1baeef
Sodiey <sotkarapo23@gmail.com>
2020-12-11 15:27:59 +0100

    installed carbon and sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import './app.scss';
+import { Button } from 'carbon-components-react';
 const App = () => {
-  return <div>Smart Blox</div>;
+  return (
+    <div>
+      Hello Carbon! Well, not quite yet. This is the starting point for the
+      Carbon tutorial.
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default App;
